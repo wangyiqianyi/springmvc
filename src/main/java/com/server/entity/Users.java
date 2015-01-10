88e702bda21bdbd9855acb7df1c0952638a7146e
wangyiqianyi <wangyiqianyi@163.com>
2015-01-10 16:18:59 +0800

    java bean validation 验证

--- src/main/java/com/server/entity/Users.java
+++ src/main/java/com/server/entity/Users.java
@@ -3,6 +3,9 @@ package com.server.entity;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 /**
  * Created by taller on 15/1/8.
@@ -15,6 +18,7 @@ public class Users {
     private String age;
     private String phone;
     private String email;
+    @NotNull(message = "用户名不能为空")
     @Column(name="user_name")
     public String getUsername() {
         return username;
@@ -23,6 +27,8 @@ public class Users {
     public void setUsername(String username) {
         this.username = username;
     }
+    @NotNull(message = "密码不能为空")
+    @Size(min = 4,max = 10,message = "密码长度必须在4-10的长度")
     @Column(name="pwd")
     public String getPwd() {
         return pwd;
@@ -49,6 +55,7 @@ public class Users {
     public void setAge(String age) {
         this.age = age;
     }
+    @NotNull(message = "手机号码不能为空")
     @Column(name = "phone")
     public String getPhone() {
         return phone;
@@ -61,7 +68,7 @@ public class Users {
     public String getEmail() {
         return email;
     }
-
+    @Pattern(regexp="^[a-zA-Z0-9_]+@[a-zA-Z0-9_]+.[a-zA-Z]{2,5}?((.cn)|(.jp))?$", message="邮箱格式不正确")
     public void setEmail(String email) {
         this.email = email;
     }
